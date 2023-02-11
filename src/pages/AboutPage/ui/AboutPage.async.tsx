6a4a57ec25ae1508e9097afd2804cb2c6eff1d5a
machumak00 <misha7877@gmail.com>
2023-02-11 13:32:06 +0500

    - добавил обработчик на несуществующие маршруты и лоадер

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -1,3 +1,8 @@
 import { lazy } from 'react'
 
-export const AboutPageAsync = lazy(async () => await import('./AboutPage'))
+export const AboutPageAsync = lazy(async () => await new Promise((resolve) => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    // Для теста загрузки и лоадера
+    setTimeout(() => { resolve(import('./AboutPage')) }, 2000)
+}))
