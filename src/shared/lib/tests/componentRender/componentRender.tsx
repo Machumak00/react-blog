7f4840f09f016087597fb7e317e9a8e121ba1278
machumak00 <misha7877@gmail.com>
2023-05-06 10:24:13 +0500

    - добавил компонентный тест

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -5,29 +5,59 @@ import { I18nextProvider } from 'react-i18next'
 import { MemoryRouter } from 'react-router-dom'
 
 import { type StateSchema, StoreProvider } from '@/app/providers/StoreProvider'
+// eslint-disable-next-line machumak-plugin/layer-imports
+import { ThemeProvider } from '@/app/providers/ThemeProvider'
 import i18nForTests from '@/shared/config/i18n/i18nForTests'
 import { getRouteMain } from '@/shared/const/router'
+import { Theme } from '@/shared/const/theme'
+// eslint-disable-next-line machumak-plugin/layer-imports
+import '@/app/styles/index.scss'
 
 export interface ComponentRenderOptions {
     route?: string
     initialState?: DeepPartial<StateSchema>
     asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
+    theme?: Theme
 }
 
-export function componentRender (component: ReactNode, options: ComponentRenderOptions = {}) {
+interface TestProviderProps {
+    children: ReactNode
+    options?: ComponentRenderOptions
+}
+
+export function TestProvider (props: TestProviderProps) {
+    const {
+        children,
+        options = {}
+    } = props
+
     const {
         route = getRouteMain(),
         initialState,
-        asyncReducers
+        asyncReducers,
+        theme = Theme.LIGHT
     } = options
 
+    return (
+        <ThemeProvider initialTheme={theme}>
+            <div className={`app ${theme}`}>
+
+                <MemoryRouter initialEntries={[route]}>
+                    <StoreProvider asyncReducers={asyncReducers} initialState={initialState}>
+                        <I18nextProvider i18n={i18nForTests}>
+                            {children}
+                        </I18nextProvider>
+                    </StoreProvider>
+                </MemoryRouter>
+            </div>
+        </ThemeProvider>
+    )
+}
+
+export function componentRender (component: ReactNode, options: ComponentRenderOptions = {}) {
     return render(
-        <MemoryRouter initialEntries={[route]}>
-            <StoreProvider asyncReducers={asyncReducers} initialState={initialState}>
-                <I18nextProvider i18n={i18nForTests}>
-                    {component}
-                </I18nextProvider>
-            </StoreProvider>
-        </MemoryRouter>
+        <TestProvider options={options}>
+            {component}
+        </TestProvider>
     )
 }
